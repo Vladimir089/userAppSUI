5e9b9d1eb693332a374e0f33a4d6f48f195326d2
Vladimir089 <pickerpikcer@gmail.com>
2024-05-25 19:00:38 +0300

    Initial Commit

---
+++ userAppSUI/userAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  userAppSUIApp.swift
+//  userAppSUI
+//
+//  Created by Владимир Кацап on 25.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct userAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
